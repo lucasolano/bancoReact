55a29e9a297d00387628d83061af4310e1860521
lucasolano <46762430+lucasolano@users.noreply.github.com>
2019-08-26 16:56:15 -0300

    Update App.js
    Terceira opção de sexo ainda não funcional.

--- App.js
+++ App.js
@@ -21,11 +21,38 @@ export default class App extends Component{
       sexo:[
        {Gen: 'Feminino', valor: 1},
        {Gen: 'Masculino',valor:2},
-       {Gen: 'Não Definido', valor:3}
+       //{Gen: 'Não Definido', valor:3}
       ],
-      valorLimite: 400,
+      limite: 0,
       estudante: false,
     };
+
+    this.nomeForm = this.nomeForm.bind(this);
+    this.enviarDados = this.enviarDados.bind(this);
+
+  }
+
+  //Método que receberá o texto digitado no input "NOME"
+
+  nomeForm(texto){
+    let state = this.state;
+    state.nome = texto;
+    this.setState(state);
+
+  }
+
+  //Método que será chamado quando apertar o botão "ABRIR CONTA"
+
+  enviarDados(){
+
+    alert(
+      'Bem Vindo ao Banco React, sua conta foi aberta com sucesso !! \n \n'+
+      'Nome: '+this.state.nome +'\n'+
+      'Sexo: '+this.state.sexo[this.state.sex].Gen+'\n'+
+      'Limite Conta: '+this.state.limite.toFixed(2)+'\n'+
+      'Conta Estudante: '+ ((this.state.estudante)? 'Ativo':'Inativo')
+    );
+
   }
 
 
@@ -46,13 +73,13 @@ export default class App extends Component{
 
     <View style={styles.inputArea}>
       <TextInput style={styles.inputNome} 
-      placeholder="Informe seu nome" 
-      underlineColorAndroid ="transparent"/>
-      onChangeText={this.nomeForm}
+      placeholder="Informe seu nome: " 
+      underlineColorAndroid ="transparent"
+      onChangeText={this.nomeForm}/>
     </View>
 
     <View style = {styles.areaTexto}>
-      <Text style={styles.textoNome}>Sexo:</Text>
+      <Text style={styles.textoNome}>Sexo: </Text>
     </View>
 
     <View style={styles.areaSexo}>
@@ -87,13 +114,21 @@ export default class App extends Component{
      
         
       <View style={styles.areaEstudante}>
-        <Switch value={this.state.estudante} onValueChange={(valorSwitch) => this.setState({status:valorSwitch})}
-        thumbColor="#000000"
-        trackColor="FF0000"
+        <Switch value={this.state.estudante} 
+                onValueChange={(valorSwitch) => this.setState({estudante:valorSwitch})}
+                onTintColor="#00c300"
+                
         />
-        <Text>{(this.state.status)? "Sim":"Não"}</Text>
     </View>
-    </View>
+
+      <View style = {styles.areaBotao}>
+        <TouchableOpacity style={styles.botao} 
+        onPress={this.enviarDados}
+        underlayColor= '#000000'>  
+          <Text style={styles.botaoTexto}> Abrir Conta </Text>  
+        </TouchableOpacity>
+      </View>
+   </View>
     
 
     );
@@ -101,29 +136,29 @@ export default class App extends Component{
 }
 
 const styles = StyleSheet.create({
-  container:{
-    paddingTop:20,
-    flex:1,
+  container: {
+    paddingTop: 20,
+    flex: 1,
   },
   logo:{
     textAlign: 'center',
-    fontSize:'30',
+    fontSize: 30,
     fontWeight:'bold',
-    color:'#A9A9A9'
+    color:'#A9A9A9',
   },
   areaTexto:{
     alignItems:'flex-start',
     paddingTop:15,
     marginLeft:20,
-    marginBottom:-8
+    marginBottom:-8,
   },
   inputArea:{
-    alignSelf: 'stretch'
+    alignSelf: 'stretch',
   },
   textoNome:{
     fontSize:17,
     color:'#000000',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   inputNome:{
     borderWidth:'1',
@@ -132,20 +167,23 @@ const styles = StyleSheet.create({
     color:'#000000',
     height:38,
     margin:20,
-    padding:10
+    padding:10,
   },
   areaSexo:{
-  flexDirection: 'row'
+  flexDirection: 'row',
   },
   pickerSexo:{
     flex:1,
     width:150,
   },
+  areaSlider:{
+    paddingTop:15,
+  },
   textoLimite:{
-    fontSize:17,
+    fontSize: 17,
     color:'#FF0000',
     fontWeight: 'bold',
-    paddingLeft:5
+    paddingLeft: 5,
   },
   limiteArea:{
     margin:5,
@@ -155,8 +193,27 @@ const styles = StyleSheet.create({
   areaEstudante:{
     paddingTop: 15,
     paddingLeft: 20,
-    alignItems:'flex-start'
-  }
- 
+    alignItems:'flex-start',
+  },
+  areaBotao:{
+    height:40,
+    marginBottom:25,
+    flexDirection:'row',
+  },
+  botao:{
+    flex: 1,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    borderRadius:150,
+    margin:20,
+  },
+  botaoTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
 
+  },
+ 
 });
